5c100353c1f7c65abe17ea7a6cff9d5cc3fe2bac
Denis Goloborodko <goldena_by@outlook.com>
2020-10-26 00:55:31 +0300

    Version 1.0 Playable, Help button is working, config is still stubbed out.

---
+++ Pig Game (Dice)/Controller/HelpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HelpViewController.swift
+//  Pig Game (Dice)
+//
+//  Created by Denis Goloborodko on 10/26/20.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+  
+    @IBAction func BackButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
