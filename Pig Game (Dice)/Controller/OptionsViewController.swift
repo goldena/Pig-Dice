57a33b15940902cbc5f4c4ca4961b69d6f2ba86f
Denis Goloborodko <goldena_by@outlook.com>
2020-11-01 00:26:28 +0300

    Version: 1.1 Alpha: Options screen stub added, slight changed in the UI.

---
+++ Pig Game (Dice)/Controller/OptionsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OptionsViewController.swift
+//  Pig Game (Dice)
+//
+//  Created by Denis Goloborodko on 11/1/20.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+    
+    @IBAction func BackButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
