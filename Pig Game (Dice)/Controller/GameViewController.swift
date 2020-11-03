d02dde4608b48fd348998a604272df6a60f0c73b
Denis Goloborodko <goldena_by@outlook.com>
2020-11-03 22:24:31 +0300

    Version: 1.05 Alpha Changes: Displaying names was fixed. 		Info about changes taking effect after a new game starts was added. Todo: 	Add localizations. Known bugs: some issues with the autolayout on very narrow/small screens.

--- Pig Game (Dice)/Controller/GameViewController.swift
+++ Pig Game (Dice)/Controller/GameViewController.swift
@@ -15,6 +15,9 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        game.newGame()
+        updateUI()
     }
             
     @IBAction func NewGameButtonPressed(_ sender: UIButton) {
