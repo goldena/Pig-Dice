5b2a150868e8de2aad20f5dc25fd9092417053d3
Denis Goloborodko <goldena_by@outlook.com>
2021-02-25 22:38:24 +0300

    Version: 1.45.4 Pre-Release Changes: Finished with unit tests for now. Fixes:	- Plans: 	Make AI smarter - more risk-prone at the beginning and if it is about to loose. 		Add animations for the dice throw. 		Change color of the buttons for the second player, to make switching from one player to another more obvious. 		Add another dice game with different rules. 		Add themes. 		Check input data validation within the Options screen. 		Add another layout for two players (one is upside down against the other one). 		Network game mode? 		Add game rules constructor for new types of game? Known bugs: minor issues with the auto-layout on very narrow/small screens.

--- Pig Dice GameTests/Pig_Dice_GameTests.swift
+++ Pig Dice GameTests/Pig_Dice_GameTests.swift
@@ -108,7 +108,7 @@ class Pig_1Dice_GameTests: XCTestCase {
         XCTAssertEqual(sut.activePlayer.totalScore, 0, "Round score is not 0 after 1 is thrown")
     }
     
-    func testCalculateScoreOneDice5() throws {
+    func testCalculateScoreOneDiceRandom() throws {
         let randomDiceExcept1 = Int.random(in: 2...6)
         
         sut.calculateScores(randomDiceExcept1)
@@ -121,7 +121,8 @@ class Pig_1Dice_GameTests: XCTestCase {
         XCTAssertEqual(sut.activePlayer.totalScore, randomDiceExcept1, "Total Score were not updated")
     }
 
-    func testCalculateScoreOneDice6() throws {
+    func testCalculateScoreOneDice6Twice() throws {
+        // Making Total score not zero
         sut.activePlayer.rollDice()
         sut.activePlayer.addRoundScore(sut.activePlayer.dice1!)
         sut.activePlayer.holdRoundScore()
@@ -133,5 +134,66 @@ class Pig_1Dice_GameTests: XCTestCase {
         
         XCTAssertEqual(sut.activePlayer.previousDice, sut.activePlayer.dice1, "Failed a condition for clearing total score")
         XCTAssertEqual(sut.activePlayer.previousDice, 6, "Total score was cleared for a wrong reason")
+    }
+}
+
+
+class Pig_2Dice_GameTests: XCTestCase {
+
+    // MARK: - Test(s) Lifecycle
+    
+    var sut: Game!
+    
+    override func setUp() {
+        super.setUp()
+        
+        Options.gameType = .PigGame2Dice
+        sut = Game()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        
+        super.tearDown()
+    }
+    
+    // MARK: - Test(s)
+                
+    func testCalculateScoreOneOfDice1() throws {
+        sut.calculateScores(1, Int.random(in: 2...6))
+        sut.calculateScores(Int.random(in: 2...6), 1)
+        sut.calculateScores(1, 1)
+        
+        XCTAssertEqual(sut.activePlayer.roundScore, 0, "Round score is not 0 after 1 is thrown")
+        XCTAssertEqual(sut.activePlayer.totalScore, 0, "Round score is not 0 after 1 is thrown")
+    }
+    
+    func testCalculateScoreTwoRandomDice() throws {
+        let randomDiceExcept1 = Int.random(in: 2...6)
+        let randomDiceExcept1and6 = Int.random(in: 2...5) // Not to trigger 6x2 (total and round scores go to zero)
+        
+        sut.calculateScores(randomDiceExcept1, randomDiceExcept1and6)
+        
+        XCTAssertEqual(sut.activePlayer.roundScore, randomDiceExcept1 + randomDiceExcept1and6, "Scores were not properly added to the Round Score")
+        
+        sut.activePlayer.holdRoundScore()
+        
+        XCTAssertEqual(sut.activePlayer.roundScore, 0, "Round Score were not cleared")
+        XCTAssertEqual(sut.activePlayer.totalScore, randomDiceExcept1 + randomDiceExcept1and6, "Total Score were not updated")
+    }
+
+    func testCalculateScoreTwoDiceDouble6() throws {
+        // Making Total score not zero
+        sut.activePlayer.rollDice()
+        sut.activePlayer.addRoundScore(sut.activePlayer.dice1!)
+        sut.activePlayer.holdRoundScore()
+        
+        while sut.activePlayer.totalScore != 0 {
+            sut.activePlayer.rollDice()
+            sut.calculateScores(sut.activePlayer.dice1!, sut.activePlayer.dice2!)
+        }
+        
+        XCTAssertEqual(sut.activePlayer.dice1, sut.activePlayer.dice2, "Failed a condition for clearing total score")
+        XCTAssertEqual(sut.activePlayer.dice1, 6, "Total score was cleared for a wrong reason")
     }
 }
